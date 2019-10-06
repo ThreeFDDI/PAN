670457c4333234d2f0c786c8b0142330bd196343
Jay Taylor <foosyou@gmail.com>
2019-10-05 21:56:53 -0500

    counter cleanup using len()

--- Log_parsing/csv_destinations.py
+++ Log_parsing/csv_destinations.py
@@ -5,13 +5,12 @@ input_file = "log.csv"
 daily_output = f"fqdn_list_{time.strftime('%Y%m%d')}.txt"
 master_output = "fqdn_list_master.txt"
 
-# create list of destinations
+# init lists for destinations
 destinations = []
+master = []
 
 # init counters
 total = 0
-unique = 0
-master = 0
 
 # open csv log file
 with open(input_file, "r") as log_file:
@@ -26,19 +25,13 @@ with open(input_file, "r") as log_file:
         fqdn = socket.getfqdn(row[8])
         # ignore destinations already in list
         if fqdn not in destinations:
-            unique +=1
+            # add unique destinations to list
             destinations.append(fqdn)
 
-# write fqdns to file 
+# write fqdns to daily file 
 with open(daily_output, "a+") as f:
     for i in destinations:
         f.write(i + "\n")
-
-# count total entries
-with open(daily_output, "r") as f:
-    for c, l in enumerate(f):
-        pass
-        master = c + 1
 
 read_files = glob.glob("*.txt")
 
@@ -49,6 +42,6 @@ with open(master_output, "wb") as outfile:
 
 # print stuff
 print(f"Total entries in current file: {total}")
-print(f"Unique entries in current file: {unique}")
-print(f"Total unique entries in master file: {master}")
+print(f"Unique entries in current file: {len(destinations)}")
+print(f"Total unique entries in master file: {len(master)}")
 
